c7a00b980a811f4d256fbf678a33e86cac3857fb
Pawan Hegde M <pawandhegde@gmail.com>
2020-04-18 11:01:38 +0530

    Update delete.php

--- Add_View data/delete.php
+++ Add_View data/delete.php
@@ -1,19 +1,4 @@
 <?php
-session_start();
-if($_SESSION['log']=="true")
-{
-   
-     if((time() -$_SESSION['last_login_timestamp'])>900)
-   {
-    header('location:logout.php');
-   }
-   else{
-    $_SESSION['last_login_timestamp']=time();
-   }
-}
-else {
-  header('Location:main1.php');
- }
 include 'dbconfig.php';
 $con=mysqli_connect($host,$user,$pass,$db) or die("we couldn't connect");
 $id=$_REQUEST['id'];
